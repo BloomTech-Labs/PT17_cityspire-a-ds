f69a9c3c5235a73bd54a3e270f6820b1f4bf70f9
Jisha Obukwelu <jisha@obukwelu.com>
2021-03-21 22:43:14 -0400

    Add weather endpoint

---
+++ app/external.py
@@ -0,0 +1,57 @@
+import requests
+import os
+import datetime
+from fastapi import APIRouter, HTTPException, Depends
+from pydantic import BaseModel
+from app.ml import City, validate_city
+from app.state_abbr import us_state_abbrev as abbr
+from dotenv import dotenv_values, load_dotenv
+
+router = APIRouter()
+load_dotenv()
+weather_api = os.getenv("WEATHER_API_KEY")
+
+
+# https://github.com/juhilsomaiya/API-Integrations-Python/blob/master/Weather_forecast/main.p
+@router.post('/api/temperature')
+async def current_weather(city:City):
+    """Retrieve current weather data from openweathermap
+
+    Fetch weather data from openweathermap
+    - description
+    - temperature
+    - high and low
+    - humidity
+    - wind speed
+    - real feel forecast
+    - pressure
+
+    args:
+        city: The target city
+
+    returns:
+        Dictionary that contains the requested data, which is converted
+        by fastAPI to a json object.
+
+    """
+    app_id = weather_api
+    location = validate_city(city) # {city: "New York", state: "NY" }
+    city_name = location.city + "," + location.state
+    county_code = "US"
+    api_call = 'http://api.openweathermap.org/data/2.5/weather?q=' + city_name + ',' + county_code + '&appid=' + app_id + '&mode=json&units=imperial'
+    data = requests.get(api_call)
+
+    data = data.json()
+    main = data['main']
+    today = datetime.datetime.today()
+    return {
+        "Date": today.strftime("%m/%d/%y"),
+        "Description": data['weather'][0]['description'],
+        "Temperature": str(main['temp'])+" F\N{DEGREE SIGN}",
+        "High Today": str(main['temp_max'])+" F\N{DEGREE SIGN}",
+        "Low Today": str(main['temp_min'])+" F\N{DEGREE SIGN}",
+        "Humidity": str(main['humidity'])+ "%",
+        "Wind Speed": str(data['wind']['speed'])+ " mph",
+        "Feels Like": str(main['feels_like'])+" F\N{DEGREE SIGN}",
+        "Pressure": str(main['pressure'])+" hPa"
+    }
